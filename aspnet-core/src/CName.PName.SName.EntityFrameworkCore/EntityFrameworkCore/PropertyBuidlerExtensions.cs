73228c0d6cb6807d835f6f4d0b9d7c5c7c06114f
王伯承 <personball@163.com>
2020-08-10 17:25:31 +0800

    add PropertyBuidlerExtensions

---
+++ aspnet-core/src/CName.PName.SName.EntityFrameworkCore/EntityFrameworkCore/PropertyBuidlerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+
+namespace CName.PName.SName.EntityFrameworkCore
+{
+    public static class PropertyBuidlerExtensions
+    {
+        public static void HasColumnTypeJsonb<TDeserializeType>(this PropertyBuilder<TDeserializeType> builder)
+        {
+            builder.HasColumnType("jsonb")
+                .HasConversion(b => JsonConvert.SerializeObject(b), b => JsonConvert.DeserializeObject<TDeserializeType>(b));
+        }
+    }
+}
